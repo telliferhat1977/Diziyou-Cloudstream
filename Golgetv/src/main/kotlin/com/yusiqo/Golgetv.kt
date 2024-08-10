466d475adbb3e2ad8b41672c9ac24753d3493eb9
Amiqo09 <106442600+Amiqo09@users.noreply.github.com>
2024-08-10 23:54:43 +0400

    edit

--- Golgetv/src/main/kotlin/com/yusiqo/Golgetv.kt
+++ Golgetv/src/main/kotlin/com/yusiqo/Golgetv.kt
@@ -35,7 +35,7 @@ class Golgetv : MainAPI() {
                     LiveSearchResponse(
                         name      = channelname,
                         url       = LoadData(streamurl, channelname, posterurl, chGroup, nation).toJson(),
-                        apiName   = this@CanliTV.name,
+                        apiName   = this@Golgetv.name,
                         type      = TvType.Live,
                         posterUrl = posterurl,
                         lang      = nation
@@ -61,7 +61,7 @@ class Golgetv : MainAPI() {
             LiveSearchResponse(
                 name      = channelname,
                 url       = LoadData(streamurl, channelname, posterurl, chGroup, nation).toJson(),
-                apiName   = this@CanliTV.name,
+                apiName   = this@Golgetv.name,
                 type      = TvType.Live,
                 posterUrl = posterurl,
                 lang      = nation
@@ -97,7 +97,7 @@ class Golgetv : MainAPI() {
                 recommendations.add(LiveSearchResponse(
                     name      = rcChannelName,
                     url       = LoadData(rcStreamUrl, rcChannelName, rcPosterUrl, rcChGroup, rcNation).toJson(),
-                    apiName   = this@CanliTV.name,
+                    apiName   = this@Golgetv.name,
                     type      = TvType.Live,
                     posterUrl = rcPosterUrl,
                     lang      = rcNation,
