05583b2cda8af381faff2667a5e1f960a8fa4d8c
telliferhat1977 <telliferhat77@gmail.com>
2026-03-16 15:57:48 +0300

    Add MyPluginPlugin class for Cloudstream integration

--- BenimSinema/src/main/kotlin/com/example/MyPluginPlugin.kt
+++ BenimSinema/src/main/kotlin/com/example/MyPluginPlugin.kt
@@ -1,1 +1,12 @@
+package com.example
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class MyPluginPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(MyPlugin()) // MyPlugin.kt'deki sınıf isminle aynı olmalı
+    }
+}
