c683d80dbedc732a9cef80d7717ecce20636baef
telliferhat1977 <telliferhat77@gmail.com>
2026-03-16 16:17:23 +0300

    Configure cloudstream plugin in build.gradle.kts
    Added configuration for cloudstream plugin with metadata.

--- BenimSinema/build.gradle.kts
+++ BenimSinema/build.gradle.kts
@@ -1,1 +1,18 @@
+version = 1
 
+cloudstream {
+    authors     = listOf("yusiqo", "keyiflerolsun")
+    language    = "tr"
+    description = "Üşenmeden Rectv Den Canlı olarak film dizi çeken eklenti, lordun elinden"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie")
+    iconUrl = "https://rectvapk.cc/favicon.ico"
+}
